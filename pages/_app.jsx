d010be176eeafae8f39c445101db0e5d81c72606
leaser019 <itcsiu21136@student.hcmiu.edu.vn>
2025-02-02 22:54:26 +0700

    add: integrate @vercel/analytics for enhanced tracking and analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,6 +2,7 @@ import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import '@/styles/globals.css';
 import { ThemeProvider } from 'next-themes';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }) {
       <main className="land relative z-10">
         <Header />
         <Component {...pageProps} />
+        <Analytics />
         <Footer />
       </main>
     </ThemeProvider>
